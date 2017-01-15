baa6d6c8a59c3647137acc7fa8af057629ee0b7e
Behshad Sebthosseini <behshad.sebt@gmail.com>
2017-01-15 02:13:58 -0500

    Website V1.0

---
+++ js/mine.js
@@ -0,0 +1,32 @@
+
+
+window.onload = function(){
+  var name1 = (document.getElementsByClassName('projoneb'));
+  name1[0].onClick = function(){
+    // alert('clicked');
+  var i = 100;
+  for(i; i >= 0; i = i-1){
+  name[0].style.height = 20 + 'px';
+  name[1].style.height = 20 + 'px';
+
+    console.log('clicked');
+  }
+  }
+}
+function myfunction1(){
+  var name =   (document.getElementsByClassName('projtwob'));
+
+  if (name[0].style.height == 0){
+
+  }else{
+    for(var i=100; i >= 0; i = i-1){
+  var name =   (document.getElementsByClassName('projtwob'));
+    name[0].style.height = i + '%';
+    name[1].style.height = i + '%';
+    name[0].style.display = "none";
+    name[2].style.height = i + '%';
+    name[2].style.display = "none";
+    name[1].style.display = "none";
+    }
+  }
+}
