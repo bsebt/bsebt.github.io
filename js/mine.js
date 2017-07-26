3ef8424fcd56921f393318e53c98755f88ae2fbe
Behshad Sebthosseini <behshad.sebt@gmail.com>
2017-07-25 23:52:31 -0400

    new navbar and about me

--- js/mine.js
+++ js/mine.js
@@ -1,3 +1,12 @@
 if (window.width < 500) {
   $("#padzero").addclass("marginzero paddingzero")
 }
+// Add slideDown animation to Bootstrap dropdown when expanding.
+  $('.dropdown-menu').on('show.bs.dropdown', function() {
+    $(this).find('.dropdown-menu').first().stop(true, true).slideDown();
+  });
+
+  // Add slideUp animation to Bootstrap dropdown when collapsing.
+  $('.dropdown-menu').on('hide.bs.dropdown', function() {
+    $(this).find('.dropdown-menu').first().stop(true, true).slideUp();
+  });
