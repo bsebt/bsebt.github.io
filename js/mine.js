99bb46f54ab7a711ed5118108b39265aa5d5d46a
Behshad Sebthosseini <behshad.sebt@gmail.com>
2017-07-31 19:12:42 -0400

    fixed background image, scrolling elements

--- js/mine.js
+++ js/mine.js
@@ -10,3 +10,9 @@ if (window.width < 500) {
   $('.dropdown-menu').on('hide.bs.dropdown', function() {
     $(this).find('.dropdown-menu').first().stop(true, true).slideUp();
   });
+
+
+  var $logo = $('body');
+  $(document).scroll(function() {
+      $logo.css({display: $(this).scrollTop() > 100? $logo.css("background: url(../img/showOff1.jpg) no-repeat top center fixed;"):$logo.css("background: url(../img/DSC05035.jpg) no-repeat top center fixed;")});
+  });
